9cfd4faa35ed017650e7bf7b4ec033c21f7ca944
Alex Ngundji <alexngundji@gmail.com>
2020-06-30 02:03:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -174,16 +174,17 @@ end
 
 
 def player_stats(player_name)
-
-  game_hash.values.each do |team_info|
-    team_info[:players].each do |player|
-      if player.has_value(player_name)
-         player.delete(:player_name) 
-         return player
+  game_hash.each do |location, team|
+    team.each do |attribute, data|
+     if attribute == :players
+       data.each do |player, stats|
+        if player == player_name
+          return stats
+        end
+       end
       end
     end
   end
-
 end
 
 
